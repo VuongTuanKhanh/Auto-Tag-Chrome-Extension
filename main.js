e87d30287ac2391301b708e09040b46d6fd3f4eb
Archius Vương <khanhvuongtuan37@gmail.com>
2020-07-08 00:50:28 +0700

    Access all chatbox

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-alert('Running');
-collection = document.getElementsByClassName('btn-tag-item');
-for (let i = 0; i < collection.length; ++i) {
-    collection[i].click();
-    //await new Promise(r => setTimeout(r, 1000));
-    setTimeout(function(){}, 2000);
+// Truy cập từng cuộc hội thoại
+function _access_All_Conversations(class_Name) {
+    let all_Conversation = document.getElementsByClassName(class_Name);
+    return all_Conversation;
 }
 
-delete collection;
+// Xử lý vòng lặp
+while(true) {
+    // Biến lưu trữ giá trị tất cả các cuộc hội thoại đã được load
+    let all_Conversation = _access_All_Conversations('conversation-list-item');
+
+    // Xử lý cho từng cuộc hội thoại
+    for(i in all_Conversation) {
+        // In đối tượng ra màn hình Console
+        console.log(all_Conversation[i]);
+        // Click vào đối tượng
+        all_Conversation[i].click();
+    }
+
+    // Dừng vòng lặp
+    break;
+}
