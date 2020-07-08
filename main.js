c337684ece93890f13ba77284c18994fbfa4dec5
Archius Vương <khanhvuongtuan37@gmail.com>
2020-07-08 12:53:44 +0700

    Reset Conversations

--- main.js
+++ main.js
@@ -4,18 +4,25 @@ function _access_All_Conversations(class_Name) {
     return all_Conversation;
 }
 
+// Chọn button tin nhắn
+document.querySelector('ul.sidebar-nav').childNodes[3].click();
+
 // Xử lý vòng lặp
 while(true) {
+    // Bật lọc tin nhắn chưa đọc
+    document.querySelector('ul.sidebar-nav').childNodes[1].click();
+
     // Biến lưu trữ giá trị tất cả các cuộc hội thoại đã được load
     let all_Conversation = _access_All_Conversations('conversation-list-item');
 
     // Xử lý cho từng cuộc hội thoại
     for(i of all_Conversation) {
-        // In đối tượng ra màn hình Console
-        console.log(i);
         // Click vào đối tượng
         i.click();
     }
+
+    // Tắt lọc tin nhắn chưa đọc
+    document.querySelector('ul.sidebar-nav').childNodes[1].click();
 
     // Dừng vòng lặp
     break;
