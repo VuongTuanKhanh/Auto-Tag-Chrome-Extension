beb80c71a7ab4f9f75d1650c7e44fdcb7c71cc5c
Archius Vương <khanhvuongtuan37@gmail.com>
2020-07-10 12:40:15 +0700

    Generate Async Functions

--- main.js
+++ main.js
@@ -1,29 +1,141 @@
-// Truy cập từng cuộc hội thoại
-function _access_All_Conversations(class_Name) {
+// Khai báo Time Range
+const limit = 1500;
+
+// Khai báo một brand Object
+function _brand(name, max = 5) {
+    this.name = name;
+    this.current_TV = 0;
+    this.max = max;
+}
+console.log('- Đã tạo Constructor')
+
+// Biến lưu giữ các Button
+let buttons;
+console.log('- Đã khai báo biến lưu trữ các nút');
+
+// Khai báo danh sách TV
+let TV = [];
+console.log('- Đã khai báo danh sách TV');
+
+// Tạo nút Untagged
+let Untagged;
+console.log('- Đã tạo nút Untagged');
+
+// Tạo danh sách các brand hiện tại
+let brands = [
+    brand_1 = new _brand('Brand 1'),
+    brand_2 = new _brand('Brand 2'),
+]
+console.log('- Đã tạo Brand List')
+
+// Hàm Async
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Chọn button tin nhắn
+async function Click_Button_Tin_Nhan() {
+    // await sleep(limit);
+    document.querySelector('ul.sidebar-nav').childNodes[3].click();
+    console.log('- Chọn Button Tin nhắn');
+}
+
+// Click vào đoạn hội thoại đầu tiên để lấy danh sách các button
+async function Chon_Hoi_Thoai_Dau_Tien() {
+    // await sleep(limit);
+    document.getElementsByClassName('conversation-list-item')[0].click()
+    console.log('- Chọn đoạn hội thoại đầu tiên để lấy danh sách các button');
+}
+
+// Lấy toàn bộ button
+async function Lay_Cac_Button() {
+    // await sleep(limit);
+    buttons = document.getElementsByClassName('btn-tag-item');
+    console.log('- Đã lấy các button', buttons);
+}
+
+// Đẩy các TV vào danh sách và tạo nút Untagged
+async function Them_TV_Vao_Danh_Sach() {
+    // await sleep(limit);
+    for (const i in buttons) {
+        // Kiểm tra tag có là TV
+        try {
+            if (buttons[i].textContent.split(' ')[0] == 'TV') {
+                TV.push(buttons[i]);
+                console.log(' + Đã đẩy', buttons[i].textContent);
+            }
+
+            // Tạo nút Untagged
+            if (buttons[i].textContent == 'Untagged') {
+                Untagged = buttons[i];
+                console.log(' + Đã tạo nút Untagged');
+            }
+        } catch {}
+    }
+    console.log('- Untagged Button:', Untagged);
+}
+
+// Lọc thẻ
+async function Loc_The() {
+    // await sleep(limit);
+    document.getElementsByClassName('btn-show-multi-filter-tag set_font_family')[0].click()
+    console.log('- Đã Click Lọc thẻ');
+}
+
+// Lọc thẻ hội thoại
+async function Loc_The_Hoi_Thoai() {
+    // await sleep(limit);
+    document.getElementsByClassName('ant-dropdown-menu-submenu-title')[0].click()
+    console.log('- Đã Click lọc hội thoại');
+}
+
+// Lọc thẻ Untagged
+async function Loc_The_Untagged() {
+    // await sleep(limit);
+    document.getElementsByClassName('item-tag-conv')[10].click()
+    console.log('- Đã lọc thẻ Untagged');
+}
+
+// Xác nhận thẻ
+async function Xac_Nhan_The() {
+    // await sleep(limit);
+    document.getElementsByClassName('btn-filter-tags')[1].click()
+    console.log('- Đã Click OK');
+}
+
+// Lọc tin nhắn chưa đọc
+async function Loc_Tin_Nhan_Chua_Doc() {
+    // await sleep(limit);
+    document.querySelector('ul.sidebar-nav').childNodes[1].click();
+    console.log('- Đã lọc tin nhắn chưa đọc');
+}
+
+// Truy cập tát cả cuộc hội thoại
+async function _access_All_Conversations(class_Name) {
+    // await sleep(limit);
     let all_Conversation = document.getElementsByClassName(class_Name);
     return all_Conversation;
 }
 
-// Chọn button tin nhắn
-document.querySelector('ul.sidebar-nav').childNodes[3].click();
-
-// Xử lý vòng lặp
-while(true) {
-    // Bật lọc tin nhắn chưa đọc
-    document.querySelector('ul.sidebar-nav').childNodes[1].click();
-
-    // Biến lưu trữ giá trị tất cả các cuộc hội thoại đã được load
-    let all_Conversation = _access_All_Conversations('conversation-list-item');
-
-    // Xử lý cho từng cuộc hội thoại
-    for(i of all_Conversation) {
-        // Click vào đối tượng
-        i.click();
-    }
-
-    // Tắt lọc tin nhắn chưa đọc
-    document.querySelector('ul.sidebar-nav').childNodes[1].click();
-
-    // Dừng vòng lặp
-    break;
+async function main() {
+    Click_Button_Tin_Nhan();
+    await sleep(limit);
+    Chon_Hoi_Thoai_Dau_Tien();
+    await sleep(limit);
+    Lay_Cac_Button();
+    await sleep(limit);
+    Them_TV_Vao_Danh_Sach();
+    await sleep(limit);
+    Loc_The();
+    await sleep(limit);
+    Loc_The_Hoi_Thoai();
+    await sleep(limit);
+    Loc_The_Untagged();
+    await sleep(limit);
+    Xac_Nhan_The();
+    await sleep(limit);
+    Loc_Tin_Nhan_Chua_Doc();
+    await sleep(limit);
 }
+
+main();
---
+++ temp.js
@@ -0,0 +1,80 @@
+let all_Conversation;
+
+let flat = true;
+
+let temp = 0;
+
+// Xử lý vòng lặp
+while (temp < 5) {
+    console.log('Vòng lặp');
+    // Bật lọc tin nhắn chưa đọc
+    document.querySelector('ul.sidebar-nav').childNodes[1].click();
+
+    // Biến lưu trữ giá trị tất cả các cuộc hội thoại đã được load
+    all_Conversation = _access_All_Conversations('conversation-list-item');
+    console.log('- Đã lấy danh sách hội thoại');
+
+    // Xử lý cho từng cuộc hội thoại
+    for (let i of all_Conversation) {
+        // Truy cập vào đoạn hội thoại
+        i.click();
+        console.log('- Click vào đoạn hội thoại thứ', i);
+
+        // Kiểm tra số lượng tab, nếu khách mới thì sẽ là 1
+        if (i.lastChild.lastChild.firstChild.firstChild.children.length == 2) {
+
+            if (flat) {
+                flat = false;
+                continue;
+            }
+
+            console.log('- Vòng lặp kiểm tra Length', i.lastChild.lastChild.firstChild.firstChild.children.length);
+
+            for (let brand of brands) {
+                console.log('- Brand hiện tại:', i.lastChild.lastChild.firstElementChild.firstChild.firstChild.textContent);
+
+                for (let tag of i.lastChild.lastChild.firstElementChild.firstChild.children) {
+                    console.log('- Tag hiện tại:', tag.textContent);
+
+                    if (tag.textContent == brand.name) {
+
+                        console.log(tag.textContent, '==', brand.name)
+
+                        // Click vào TV hiện tại của brand
+                        TV[brand.current_TV].click();
+
+                        console.log('- Chọn', TV[brand.current_TV].textContent);
+                        console.log('- Click:', brand.name, 'Length:', i.lastChild.lastChild.firstChild.firstChild.children.length, 'Current:', brand.current_TV)
+
+                        flat = false;
+
+                        // Bỏ chọn Untagged
+                        try {
+                            Untagged.click();
+                            console.log('- Bỏ chọn Untagged');
+                        } catch {};
+
+                        // Ấn chưa đọc
+                        document.querySelectorAll('.chat-menu-bar>li')[4].click()
+                        console.log('- Ấn chưa đọc')
+
+                        // Tăng TV của brand lên 1
+                        brand.current_TV += 1;
+
+                        // Kiểm tra TV có vượt quá số lượng quy định
+                        if (brand.current_TV == brand.max) {
+                            brand.current_TV = 0;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // Tắt lọc tin nhắn chưa đọc
+    document.querySelector('ul.sidebar-nav').childNodes[1].click();
+
+    // Sleep
+    temp++;
+}
